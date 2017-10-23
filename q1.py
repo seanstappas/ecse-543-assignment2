c5aeaf06e73de5755574699ee140a83c70eedf18
Sean Stappas <seanstappas@gmail.com>
2017-10-23 09:16:27 -0400

    Built local S for both triangles

--- q1.py
+++ q1.py
@@ -2,8 +2,15 @@ from finite_element import Triangle, find_local_S_matrix
 
 
 def q1():
-    x = [0, 0, 0.02]
-    y = [0.02, 0, 0]
+    build_triangle_and_find_local_S(
+        [0, 0, 0.02],
+        [0.02, 0, 0])
+    build_triangle_and_find_local_S(
+        [0.02, 0, 0.02],
+        [0.02, 0.02, 0])
+
+
+def build_triangle_and_find_local_S(x, y):
     triangle = Triangle(x, y)
     S = find_local_S_matrix(triangle)
     print('S matrix: {}'.format(S))
